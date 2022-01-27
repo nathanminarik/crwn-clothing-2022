51d49ff1881e7a5269497a240ef057a06d7d73ce
Nathanael Minarik <nathanaelminarik@Nathanaels-MacBook-Pro.local>
2022-01-27 16:53:21 -0700

    Handle sign in with email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { signInWithGoogle } from './../../firebase';
+import { auth, signInWithGoogle } from './../../firebase';
 import { FormInput } from '../form-input';
 import { CustomButton } from '../custom-button';
 import './sign-in.styles.scss';
@@ -14,10 +14,17 @@ export class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
