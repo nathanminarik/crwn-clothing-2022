6d670590e76014a2dd31ecdde6ff1d7bde7f2ae7
Nathanael Minarik <nathanaelminarik@Nathanaels-MacBook-Pro.local>
2022-01-27 13:19:54 -0700

    Redo archetecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
-import { HomePage } from './homepage.component';
+import { HomePage } from './pages';
+import './App.scss';
 
 function App() {
   return <HomePage />;
