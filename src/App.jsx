7fcbd853129573773a2394f4e78ddcd1eba462cc
Nathanael Minarik <nathanaelminarik@Nathanaels-MacBook-Pro.local>
2022-01-27 17:49:41 -0700

    Added state to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.scss';
 import { Header } from './components';
@@ -41,14 +41,24 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/authenticate" component={Authenticate} />
+          <Route
+            exact
+            path="/authenticate"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <Authenticate />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
